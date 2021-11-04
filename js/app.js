48eebe97fa87a8135f9d773087203fc6362550d6
mbpA1278 <cloudubuntuwork@gmail.com>
2021-11-04 18:53:20 -0300

    complete la funcionalidad del ejercicio

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
 
 let cont=document.querySelector('#cont');
-
-
-
-
+let contador=1
 
 function agregar(){
     let input=document.querySelector('#input').value;
-    cont.innerHTML+=`<div>
+    cont.innerHTML+=`<div id="div${contador}">
     <p>
      ${input}
     </p>
-    <button class="btn btn-danger" type="button" onclick="eliminar()">
+    <button class="btn btn-danger" type="button" onclick="eliminar('div${contador}')">
       Eliminar
     </button>
     <hr />
   </div>`
+  contador++;
 }
 
-function eliminar(){
-  cont.removeChild(cont.lastChild);
+function eliminar(div){
+  let element=document.querySelector('#'+div);
+  cont.removeChild(element);
 }
