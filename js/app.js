affb8669df79c270a6ff4e83c332dd6410778727
alan <cloudubuntuwork@gmail.com>
2021-11-04 15:18:06 -0300

    unas pruebas mas

--- js/app.js
+++ js/app.js
@@ -7,15 +7,17 @@ let cont=document.querySelector('#cont');
 
 function agregar(){
     let input=document.querySelector('#input').value;
-    cont.innerHTML+=`<p>
-    ${input}
-  </p>
-  <button class="btn btn-danger" type="button" onclick="eliminar()">
-    Eliminar
-  </button>
-  <hr />`
+    cont.innerHTML+=`<div>
+    <p>
+     ${input}
+    </p>
+    <button class="btn btn-danger" type="button" onclick="eliminar()">
+      Eliminar
+    </button>
+    <hr />
+  </div>`
 }
 
 function eliminar(){
-  
+  cont.removeChild(cont.lastChild);
 }
